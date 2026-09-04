2c0047ce70bb243ffae97d88c63ac58899c17522
MaxNascimento-DEV <maxwellcadete9@gmail.com>
2026-09-04 10:16:43 -0300

    feat(entity): cria entidade QrCode

---
+++ Backend/EmerTAG-APP/src/main/java/br/com/EmerTAG/EmerTAG_APP/Entity/QrCode.java
@@ -0,0 +1,42 @@
+package br.com.EmerTAG.EmerTAG_APP.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+ import java.time.LocalDateTime;
+
+ @Entity
+ @Table(name= "qr_code")
+ @Getter
+ @Setter
+ @NoArgsConstructor
+ @AllArgsConstructor
+ @Builder
+
+public class QrCode {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idQrCode;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_pessoa", nullable = false, unique = true)
+    private PessoaProtegida pessoaProtegida;   
+
+    @Column(nullable = false, unique = true, length = 255)
+    private String codigo; 
+
+    @Column(name = "url_acesso", nullable = false, length = 255)
+    private String urlAcesso;
+
+    @Column(name = "data_gerada", nullable = false, updatable = false)
+    private LocalDateTime dataGerada;
+    
+    private boolean ativo;
+
+    @PrePersist
+    protected void prePersist(){
+        this.dataGerada = LocalDateTime.now();
+        this.ativo = true;
+    }
+
+}
